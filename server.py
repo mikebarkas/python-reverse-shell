065b87df5baa21eba22f1120b23cd2771af7d8c8
Mike Barkas <mike@barkas.com>
2016-04-10 15:27:48 -0400

    Creating socket function.

--- server.py
+++ server.py
@@ -1,2 +1,15 @@
 import socket
 import sys
+
+
+def socket_create():
+    try:
+        global host
+        global port
+        global s
+        host = ''
+        port = 9999
+        s = socket.socket()
+
+    except socket.error as e:
+        print 'Error creating socket: ' + str(e)
