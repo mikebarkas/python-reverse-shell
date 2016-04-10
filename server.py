0fd2ec8879bf5757c450b531c1d20a3607a84a54
Mike Barkas <mike@barkas.com>
2016-04-10 15:43:07 -0400

    Function for binding socket and port information.

--- server.py
+++ server.py
@@ -13,3 +13,18 @@ def socket_create():
 
     except socket.error as e:
         print 'Error creating socket: ' + str(e)
+
+def socket_bind():
+    try:
+        global host
+        global port
+        global s
+
+        print'Binding socket and port: ' + str(port)
+        s.bind((host, port))
+        s.listen(3)
+
+    except socket.error as e:
+        print 'Binding socket error: ' + str(e)
+        print 'Retrying..'
+        socket_bind()
